b45b2c8daf66cb7d8100cdbd802cd7bb833828df
Jagath Vytheeswaran <jagathvythee@gmail.com>
2022-11-29 23:32:06 -0600

    removing beta connectors

--- monoid-integrations/monoid-amplitude/main.py
+++
@@ -1,9 +0,0 @@
-import os
-from sys import argv
-import sys
-from amplitude.amplitude_silo import AmplitudeSilo
-from monoid_pydev.runner import run_query
-
-if __name__ == "__main__":
-    integration = AmplitudeSilo()
-    run_query(integration, sys.argv[1:])
--- monoid-integrations/monoid-segment/main.py
+++
@@ -1,9 +0,0 @@
-import os
-from sys import argv
-import sys
-from segment_lib.segment_silo import SegmentSilo
-from monoid_pydev.runner import run_query
-
-if __name__ == "__main__":
-    integration = SegmentSilo()
-    run_query(integration, sys.argv[1:])
